8be90d8ba44a21a23f7c876726620c98dffb05db
Meg Rehn <mrehn87@gmail.com>
2021-11-07 16:56:07 -0500

    Formatting of string lists

--- users.tf
+++ users.tf
@@ -17,7 +17,7 @@ resource "aws_cognito_user_pool_client" "webauth-client" {
   generate_secret      = true
   explicit_auth_flows  = ["ADMIN_NO_SRP_AUTH"]
   allowed_oauth_flows  = ["implicit"]
-  allowed_oauth_scopes = ["phone","email","openid","profile","aws.cognito.signin.user.admin"]
+  allowed_oauth_scopes = ["phone", "email", "openid", "profile", "aws.cognito.signin.user.admin"]
   callback_urls        = ["www.github.com"]
 
   supported_identity_providers = [aws_cognito_identity_provider.users-identity-provider.provider_name]
