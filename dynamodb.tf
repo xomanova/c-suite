3ba681166ce5cef94aca0193c8ce21b8f0e4c640
Meg Rehn <mrehn87@gmail.com>
2021-11-07 17:30:56 -0500

    create ddb table and access policy

---
+++ dynamodb.tf
@@ -0,0 +1,53 @@
+resource "aws_dynamodb_table" "users-ddb" {
+  name             = "potential-guacamole-users"
+  billing_mode     = "PAY_PER_REQUEST"
+  hash_key         = "FirstName"
+  range_key        = "LastName"
+
+  attribute {
+    name = "FirstName"
+    type = "S"
+  }
+
+  attribute {
+    name = "LastName"
+    type = "S"
+  }
+
+  replica {
+    region_name = "us-east-2"
+  }
+}
+
+resource "aws_iam_role" "users-ddb-iam-role" {
+  name = "potential-guacamole-users-ddb-access"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "my_inline_policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = ["dynamodb:GetItem","dynamodb:PutItem","dynamodb:Query"]
+          Effect   = "Allow"
+          Resource = aws_dynamodb_table.users-ddb.id
+        },
+      ]
+    })
+  }
+}
