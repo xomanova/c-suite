08efa9e6bb01ebd85f1b4f311dc967fce327496c
Meg Rehn <mrehn87@gmail.com>
2021-11-11 15:42:05 -0500

    CNAME for API GW

--- websockets.tf
+++ websockets.tf
@@ -3,3 +3,12 @@ resource "aws_apigatewayv2_api" "websocket_api_gw" {
   protocol_type              = "WEBSOCKET"
   route_selection_expression = "$request.body.action"
 }
+
+# Route53 CNAME record
+resource "aws_route53_record" "socket" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "websockets-${var.project}.${var.aws_hosted_zone}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_apigatewayv2_api.websocket_api_gw.apiendpoint]
+}
