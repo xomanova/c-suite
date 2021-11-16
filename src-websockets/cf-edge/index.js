13e236327898d79b9f7820022397610982767a48
Meg Rehn <mrehn87@gmail.com>
2021-11-16 15:19:22 -0500

    modify edge lambda

--- src-websockets/cf-edge/index.js
+++ src-websockets/cf-edge/index.js
@@ -1,9 +1,10 @@
 exports.handler = (event, context, callback) => {
   // Get contents of request
   // Request event docs: https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/lambda-event-structure.html
-  const request = event.Records[0].cf.request;
-  const path = request.uri;
-  path = "/"
+  var request = event.Records[0].cf.request;
+  const req_json = JSON.parse(request);
+  req_json['uri'] = "/"
+  var req_mod = JSON.stringify(req_json);
   // Return modified request
-  callback(null, request);
+  callback(null, req_json);
 };
