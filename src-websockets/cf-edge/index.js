5d0e8711c6c008c813a5e8642f070475d7a62ab6
Meg Rehn <mrehn87@gmail.com>
2021-11-16 15:58:14 -0500

    clear uri request path at edge before forwarding to gw

--- src-websockets/cf-edge/index.js
+++ src-websockets/cf-edge/index.js
@@ -1,16 +1,13 @@
+// Request event docs: https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/lambda-event-structure.html
+'use strict';
+
 exports.handler = (event, context, callback) => {
-  // Get contents of request
-  // Request event docs: https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/lambda-event-structure.html
-  console.log("The raw cloudfront event is <BEGIN>" + event + "<END>");
+    const request = event.Records[0].cf.request;
+    let wssUri;
+    wssUri = '/'
 
-  var request = event.Records[0].cf.request;
-  console.log("The var request is <BEGIN>" + request + "<END>");
-
-  const req_json = JSON.parse(request);
-  req_json['uri'] = "/"
-  var req_mod = JSON.stringify(req_json);
-
-  console.log("The var req_mod is <BEGIN>" + req_mod + "<END>");
-  // Return modified request
-  callback(null, req_mod);
+    // clear request URI for websocket api gateway
+    request.uri = wssUri;
+    console.log(`Request uri set to "${request.uri}"`);
+    callback(null, request);
 };
