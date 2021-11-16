2f0e4714097983579be3a09ce733fdcc48a82dea
Meg Rehn <mrehn87@gmail.com>
2021-11-15 22:50:14 -0500

    index -> exports in node.js

--- src-websockets/cf-edge/index.js
+++ src-websockets/cf-edge/index.js
@@ -1,4 +1,4 @@
-index.handler = (event, context, callback) => {
+exports.handler = (event, context, callback) => {
   // Get contents of request
   // Request event docs: https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/lambda-event-structure.html
   const request = event.Records[0].cf.request;
