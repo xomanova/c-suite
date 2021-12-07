afc3b8bf10f40473de27fa3b38c3f87e88e9c184
Meg Rehn <mrehn87@gmail.com>
2021-12-06 22:32:41 -0500

    formatting

--- websockets-lambda.tf
+++ websockets-lambda.tf
@@ -192,7 +192,7 @@ resource "aws_sns_topic_subscription" "event_topic_lambda" {
 resource "aws_lambda_permission" "with_sns" {
     statement_id  = "AllowExecutionFromSNS"
     action        = "lambda:InvokeFunction"
-    function_name = "${aws_lambda_function.sendmessage_lambda.arn}"
+    function_name = "${aws_lambda_function.sendmessage_lambda.function_name}"
     principal     = "sns.amazonaws.com"
     source_arn    = "${aws_sns_topic.event_topic.arn}"
 }
