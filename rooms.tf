979d86cec83b63530c82a90b51f39a6985e31e1b
Meg Rehn <mrehn87@gmail.com>
2021-12-10 21:28:21 -0500

    add new file rooms.tf Error: Reference to undeclared resource

---
+++ rooms.tf
@@ -0,0 +1,13 @@
+# Create websockets dynamodb
+resource "aws_dynamodb_table" "rooms_ddb" {
+  name             = "${var.project}-rooms"
+  hash_key         = "room_id"
+  billing_mode     = "PAY_PER_REQUEST"
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+
+  attribute {
+    name = "room_id"
+    type = "S"
+  }
+}
