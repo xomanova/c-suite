88c445aa79bf279740c2f5e26531fd984774a368
Meg Rehn <mrehn87@gmail.com>
2021-12-27 00:21:51 -0500

    state test

--- src/js/room.js
+++ src/js/room.js
@@ -359,7 +359,8 @@
 
   netgames.render = function(state, players) {
     var $content, $section, $spectator_base, $utility_menu, phase, player, ref, spectator;
-
+    state.phase = 'huddle';
+    
     console.log("logging from netgames.render room.js:363, state: " + state );
     console.log("logging from netgames.render room.js:364, state.phase: " + state.phase );
 
