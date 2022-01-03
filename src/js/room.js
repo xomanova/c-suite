f677143c639cfc0b8c31037cfde0c14a61fe1ee8
Meg Rehn <mrehn87@gmail.com>
2022-01-02 23:09:08 -0500

    json parse players inside render

--- src/js/room.js
+++ src/js/room.js
@@ -360,7 +360,7 @@
   netgames.render = function(state, players) {
     var $content, $section, $spectator_base, $utility_menu, phase, player, ref, spectator;
     state = JSON.parse(state);
-    //players = JSON.parse(players);
+    players = JSON.parse(players);
     console.log("logging from netgames.render room.js:362, state typeof: " + typeof state );
     console.log("logging from netgames.render room.js:363, state: " + state );
     console.log("logging from netgames.render room.js:364, state.phase: " + state.phase );
