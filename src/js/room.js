e088e77a3471c8d4a1a06160fc4cef1410a7346f
Meg Rehn <mrehn87@gmail.com>
2022-01-04 19:48:21 -0500

    Uncaught ReferenceError: windows is not defined

--- src/js/room.js
+++ src/js/room.js
@@ -1376,7 +1376,7 @@
     });
     if (netgames.player.name != null) {
       join_room();
-      windows.location.replace("room.html")
+      window.location.replace("room.html")
     } else {
       //if (URLSearchParams) {
       //  url_params = new URLSearchParams(window.location.search);
