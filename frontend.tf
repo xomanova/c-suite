bce3b3daeffdda9896fa38fa659f0288918b109d
Meg Rehn <mrehn87@gmail.com>
2023-10-17 18:40:11 -0400

    thanks for complicating s3 bucket acls

--- frontend.tf
+++ frontend.tf
@@ -6,6 +6,27 @@ resource "aws_s3_bucket" "www_bucket" {
 resource "aws_s3_bucket_acl" "public_site_resources" {
   bucket = aws_s3_bucket.www_bucket.id
   acl    = "public-read"
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.public_access,
+    aws_s3_bucket_ownership_controls.ownership_control,
+  ]
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.public_site_resources.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "ownership_control" {
+  bucket = aws_s3_bucket.public_site_resources.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
 // Organize source directory into MIME type sets for upload
